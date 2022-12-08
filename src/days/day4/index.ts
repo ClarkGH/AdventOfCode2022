0b611cbfb558939023489476a001ec06f929b8e3
clarkgh <clarkhinchcliff@gmail.com>
2022-12-08 14:57:05 -0500

    Day 4 part 2

--- src/days/day4/index.ts
+++ src/days/day4/index.ts
@@ -4,13 +4,13 @@ import { readFileSync } from 'fs';
 import path, { join } from 'path';
 import { fileURLToPath } from 'url';
 
-export const getOverlappingCampAssignments = (): number => {
-    const CAMP_ASSIGNMENTS = './input.txt'
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const assignmentPairsList = readFileSync(join(__dirname, CAMP_ASSIGNMENTS), 'utf-8')
-    .split(/\r?\n/); // List of assignments. Length N.
+const CAMP_ASSIGNMENTS = './input.txt'
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const assignmentPairsList = readFileSync(join(__dirname, CAMP_ASSIGNMENTS), 'utf-8')
+.split(/\r?\n/); // List of assignments. Length N.
 
+export const countEnclosingCampAssignments = (): number => {
     if (!assignmentPairsList) return 0;
 
     /**
@@ -36,6 +36,39 @@ export const getOverlappingCampAssignments = (): number => {
         const assignment2Contains1 = parseInt(assignment1Left) >= parseInt(assignment2Left) && parseInt(assignment1Right) <= parseInt(assignment2Right);
 
         if (assignment1Contains2 || assignment2Contains1) {
+            return acc + 1;
+        }
+
+        return acc + 0;
+    }, 0);
+}
+
+export const countOverlappingCampAssignments = (): number => {
+    if (!assignmentPairsList) return 0;
+
+    /**
+     * Example assignment: '17-20,18-19'.
+     * 
+     * In example case:
+     * 17 is the start of the first assignment.
+     * 20 is the end of the first assignment.
+     * 18 is the start of the second assignment.
+     * 19 is the end of the second assignment.
+     * 
+     * In every case:
+     * IF an assignment's start is LESS THAN OR EQUAL to the other assignment's end
+     * AND if the assignment's end is GREATER THAN OR EQUAL to the other assignments start
+     * 
+     * It is overlapping.
+     */
+
+    return assignmentPairsList.reduce((acc, assignmentPairs): number => {
+        const [assignment1, assignment2]: [string, string] = assignmentPairs.split(',') as [string, string];
+        const [assignment1Left, assignment1Right]: [string, string] = assignment1.split('-') as [string, string];
+        const [assignment2Left, assignment2Right]: [string, string] = assignment2.split('-') as [string, string];
+        const assignmentsOverlap = parseInt(assignment1Left) <= parseInt(assignment2Right) && parseInt(assignment1Right) >= parseInt(assignment2Left);
+
+        if (assignmentsOverlap) {
             return acc + 1;
         }
 
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { organizedElvenCalories } from './days/day1/index.js';
 import { rockPaperScissorsScore } from './days/day2/index.js';
 import { badginator, ruckSackinator } from './days/day3/index.js';
-import { getOverlappingCampAssignments } from './days/day4/index.js'
+import { countEnclosingCampAssignments, countOverlappingCampAssignments } from './days/day4/index.js'
 
 // console.log(organizedElvenCalories()); // day 1.
 // console.log(rockPaperScissorsScore()); // day 2 part 2.
 // console.log(ruckSackinator()); // day 3 part 1.
 // console.log(badginator()); // day 3 part 2.
-console.log(getOverlappingCampAssignments()) // day 4 part 1
+// console.log(countEnclosingCampAssignments()) // day 4 part 1
+console.log(countOverlappingCampAssignments()) // day 4 part 2
