eb6d53d2c908a7fecc173abfff50c6b8a832135c
clarkgh <clarkhinchcliff@gmail.com>
2022-12-08 15:10:33 -0500

    remove unnecessary logic

--- src/days/day4/index.ts
+++ src/days/day4/index.ts
@@ -66,7 +66,7 @@ export const countOverlappingCampAssignments = (): number => {
         const [assignment1, assignment2]: [string, string] = assignmentPairs.split(',') as [string, string];
         const [assignment1Left, assignment1Right]: [string, string] = assignment1.split('-') as [string, string];
         const [assignment2Left, assignment2Right]: [string, string] = assignment2.split('-') as [string, string];
-        const assignmentsOverlap = parseInt(assignment1Left) <= parseInt(assignment2Right) && parseInt(assignment1Right) >= parseInt(assignment2Left) || parseInt(assignment2Left) <= parseInt(assignment1Right) && parseInt(assignment2Right) >= parseInt(assignment1Left);
+        const assignmentsOverlap = parseInt(assignment1Left) <= parseInt(assignment2Right) && parseInt(assignment1Right) >= parseInt(assignment2Left);
 
         if (assignmentsOverlap) {
             return acc + 1;
