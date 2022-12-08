78b89b1008a3fd5009d62203d60a00b49778fd48
clarkgh <clarkhinchcliff@gmail.com>
2022-12-08 15:28:52 -0500

    -Add day 2 part 2. -Housekeeping

--- src/days/day2/index.ts
+++ src/days/day2/index.ts
@@ -1,28 +1,80 @@
-// Solution for Day 2, part 2: https://adventofcode.com/2022/day/2
+// Solution for Day 2: https://adventofcode.com/2022/day/2
 
 import { readFileSync } from 'fs';
 import path, { join } from 'path';
 import { fileURLToPath } from 'url';
 
-const STRATEGY_GUIDE = './input.txt'
+enum PointsMap {
+    X = 1,
+    Y = 2,
+    Z = 3,
+    WIN = 6,
+    DRAW = 3,
+    LOSE = 0,
+}
 
-export const rockPaperScissorsScore = (): number => {
-    // Need to set up file/dir to read the files like node in es modules.
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    enum PointsMap {
-        X = 1,
-        Y = 2,
-        Z = 3,
-        WIN = 6,
-        DRAW = 3,
-        LOSE = 0,
-    }
+const STRATEGY_GUIDE = './input.txt';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Example output: ['C Z', ...].
+const guideList = readFileSync(join(__dirname, STRATEGY_GUIDE), 'utf-8')
+    .split(/\r?\n/);
 
-    // Example output: ['C Z', ...].
-    const guideList = readFileSync(join(__dirname, STRATEGY_GUIDE), 'utf-8')
-        .split(/\r?\n/);
+// Part 1:
+export const badElvenJankenPredictor = (): number => {
+    let estimatedScore = 0;
 
+    guideList.forEach((roundPlays) => {
+        const opponentPlay = roundPlays[0];
+        const clientPlay = roundPlays[2];
+
+        switch (opponentPlay) {
+            case 'A':
+                switch (clientPlay) {
+                    case 'X':
+                        estimatedScore += (PointsMap.DRAW + PointsMap.X);
+                        break;
+                    case 'Y':
+                        estimatedScore += (PointsMap.WIN + PointsMap.Y);
+                        break;
+                    case 'Z':
+                        estimatedScore += (PointsMap.LOSE + PointsMap.Z);
+                        break;
+                }
+                break;
+            case 'B':
+                switch (clientPlay) {
+                    case 'X':
+                        estimatedScore += (PointsMap.LOSE + PointsMap.X);
+                        break;
+                    case 'Y':
+                        estimatedScore += (PointsMap.DRAW + PointsMap.Y);
+                        break;
+                    case 'Z':
+                        estimatedScore += (PointsMap.WIN + PointsMap.Z);
+                        break;
+                }
+                break;
+            case 'C':
+                switch (clientPlay) {
+                    case 'X':
+                        estimatedScore += (PointsMap.WIN + PointsMap.X);
+                        break;
+                    case 'Y':
+                        estimatedScore += (PointsMap.LOSE + PointsMap.Y);
+                        break;
+                    case 'Z':
+                        estimatedScore += (PointsMap.DRAW + PointsMap.Z);
+                        break;
+                }
+                break;
+        }
+    });
+
+    return estimatedScore;
+}
+
+export const goodElvenJankenPredictor = (): number => {
     let estimatedScore = 0;
 
     guideList.forEach((roundPlays) => {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { organizedElvenCalories } from './days/day1/index.js';
-import { rockPaperScissorsScore } from './days/day2/index.js';
+import { badElvenJankenPredictor, goodElvenJankenPredictor } from './days/day2/index.js';
 import { badginator, ruckSackinator } from './days/day3/index.js';
 import { countEnclosingCampAssignments, countOverlappingCampAssignments } from './days/day4/index.js'
 
 // console.log(organizedElvenCalories()); // day 1.
-// console.log(rockPaperScissorsScore()); // day 2 part 2.
+console.log(badElvenJankenPredictor()); // day 2 part 1.
+console.log(goodElvenJankenPredictor()); // day 2 part 2.
 // console.log(ruckSackinator()); // day 3 part 1.
 // console.log(badginator()); // day 3 part 2.
 // console.log(countEnclosingCampAssignments()) // day 4 part 1
-console.log(countOverlappingCampAssignments()) // day 4 part 2
+// console.log(countOverlappingCampAssignments()) // day 4 part 2
